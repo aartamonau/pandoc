062e10561f634f9a5092f41d46dd37bb2f3a82df
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2013-09-04 21:48:29 -0700

    enclosed markup

--- src/Text/Pandoc/Readers/Org.hs
+++ src/Text/Pandoc/Readers/Org.hs
@@ -16,7 +16,7 @@ import Control.Applicative ( (<$>), (<*), (*>), (<|>), pure )
 import Data.Char ( toLower )
 import Data.Default ( def )
 import Data.List ( intersperse )
-import Data.Monoid ( mconcat, mempty )
+import Data.Monoid ( mconcat, mempty, (<>) )
 
 import Text.Pandoc.Builder ( Blocks, Inlines )
 import qualified Text.Pandoc.Builder as Builder
@@ -28,7 +28,7 @@ import Text.Pandoc.Parsing  ( Parser, ParserState(..),
                               notFollowedBy, eof, manyTill, many1,
                               optional, choice, skipSpaces, try, string,
                               newline, anyChar, lookAhead,
-                              nonspaceChar, spaceChar )
+                              spaceChar, nonspaceChar, enclosed, oneOf )
 
 -- | Convert Emacs Org-Mode to 'Pandoc' document.
 readOrg :: ReaderOptions        -- ^ Reader options.
@@ -63,6 +63,8 @@ header = do
 
 inline :: OrgParser Inlines
 inline = choice [ whitespace
+                , enclosure
+                , danglingEnclosure
                 , word
                 ]
 
@@ -73,8 +75,61 @@ whitespace = do
   -- don't return space in the end of lines
   (lookAhead newline *> pure mempty) <|> pure Builder.space
 
+inlineSep :: OrgParser ()
+inlineSep = (spaceChar <|> newline) *> pure ()
+
 word :: OrgParser Inlines
-word = Builder.str <$> many1 nonspaceChar
+word = Builder.str <$> many1 (notFollowedBy end *> anyChar)
+  where end = skip inlineSep <|> skip danglingEnclosure
+
+enclosureSpecs :: [(Char, OrgParser Inlines)]
+enclosureSpecs = [ spec '/' (fmap Builder.emph . enclWords)
+                   -- treat underlines as emphasis
+                 , spec '_' (fmap Builder.emph . enclWords)
+                 , spec '*' (fmap Builder.strong . enclWords)
+                 , spec '+' (fmap Builder.strikeout . enclWords)
+                 , spec '~' code
+                 , spec '=' code
+                 ]
+  where encl :: Char -> OrgParser a -> OrgParser [a]
+        encl c = enclosed start end
+          where start = char c
+                          <* notFollowedBy (skip inlineSep <|> skip (char c))
+                end = char c <* lookAhead inlineSep
+
+        enclWords :: Char -> OrgParser Inlines
+        enclWords c = mconcat <$> encl c (nonWhitespace
+                                          <|> whitespaceNonWhitespace)
+          where nonWhitespace = word <|> danglingEnclosure
+                whitespaceNonWhitespace = do
+                  wp <- whitespace
+                  nwp <- nonWhitespace
+                  return $ wp <> nwp
+
+        code :: Char -> OrgParser Inlines
+        code c = Builder.code <$> concat
+                              <$> encl c (nonWhitespace
+                                          <|> whitespaceNonWhitespace)
+          where dangling = (:[]) <$> char c <* lookAhead inlineSep
+                nonWhitespace = many1 (notFollowedBy dangling *> nonspaceChar)
+                                <|> dangling
+
+                whitespaceNonWhitespace = do
+                  wp <- many1 spaceChar
+                  nwp <- nonWhitespace
+
+                  return $ wp ++ nwp
+
+        spec :: Char -> (Char -> a) -> (Char, a)
+        spec c f = (c, f c)
+
+enclosure :: OrgParser Inlines
+enclosure = choice $ map snd enclosureSpecs
+
+danglingEnclosure :: OrgParser Inlines
+danglingEnclosure = Builder.str <$> (:[]) <$> try p
+  where chars = map fst enclosureSpecs
+        p = oneOf chars <* lookAhead inlineSep
 
 textLine :: OrgParser Inlines
 textLine = mconcat <$> manyTill inline end
